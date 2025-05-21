f81f4636673429f29ee7b5bee10d05e87c1d41cb
Vansh Saini <macbook-air@Mac.lan>
2025-05-21 15:23:15 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,118 @@
+require("dotenv").config();
+
+const express = require("express");
+const { HoldingsModel } = require("./models/HoldingsModel");
+const { PositionsModel } = require("./models/PositionModel");
+const { OrdersModel } = require("./models/OrdersModel");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/AuthRoute");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URL;
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/", authRoute);
+
+// app.get("/addPositions",(req,res)=>{
+//   let tempPositions=[
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ]
+//   for(position of tempPositions){
+//     let newPosition=new PositionsModel({
+//       product: position.product,
+//       name:  position.name,
+//       qty: position.qty,
+//       avg:  position.avg,
+//       price:  position.price,
+//       net:  position.net,
+//       day:  position.day,
+//       isLoss:  position.isLoss,
+//     })
+//     newPosition.save();
+//   }
+//   res.send("done !!!");
+
+// });
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.get("/allOrders", async (req, res) => {
+  let allOrders = await OrdersModel.find({});
+  res.json(allOrders);
+});
+
+app.post("/newOrder", async (req, res) => {
+  console.log("new order initialized");
+  let newOrder = await new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+  newOrder.save();
+  console.log(newOrder.id);
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const deletedOrder = await OrdersModel.findOneAndDelete({
+      name: req.params.id,
+    });
+    console.log(deletedOrder);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+  
+});
+
+mongoose
+  .connect(uri)
+  .then(console.log("connected to db"))
+  .catch((error) => handleError(error));
+
+app.listen(PORT, () => {
+  console.log("app is started");
+});
---
+++ dashboard/src/components/WatchList.js
@@ -0,0 +1,182 @@
+import React, { useState, useContext } from "react";
+
+import GeneralContext from "./GeneralContext";
+import axios from "axios";
+
+import { Tooltip, Grow } from "@mui/material";
+
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
+
+import { watchlist } from "../data/data";
+import { DoughnutChart } from "./DoughnoutChart";
+
+const labels = watchlist.map((subArray) => subArray["name"]);
+
+const WatchList = () => {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Price",
+        data: watchlist.map((stock) => stock.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // export const data = {
+  //   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  // datasets: [
+  //   {
+  //     label: "# of Votes",
+  //     data: [12, 19, 3, 5, 2, 3],
+  //     backgroundColor: [
+  //       "rgba(255, 99, 132, 0.2)",
+  //       "rgba(54, 162, 235, 0.2)",
+  //       "rgba(255, 206, 86, 0.2)",
+  //       "rgba(75, 192, 192, 0.2)",
+  //       "rgba(153, 102, 255, 0.2)",
+  //       "rgba(255, 159, 64, 0.2)",
+  //     ],
+  //     borderColor: [
+  //       "rgba(255, 99, 132, 1)",
+  //       "rgba(54, 162, 235, 1)",
+  //       "rgba(255, 206, 86, 1)",
+  //       "rgba(75, 192, 192, 1)",
+  //       "rgba(153, 102, 255, 1)",
+  //       "rgba(255, 159, 64, 1)",
+  //     ],
+  //     borderWidth: 1,
+  //   },
+  // ],
+  // };
+
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> {watchlist.length} / 50</span>
+      </div>
+
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
+
+      <DoughnutChart data={data} />
+    </div>
+  );
+};
+
+export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(true);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchlistActions(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchlistActions(true);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  const generalContext = useContext(GeneralContext);
+
+  const handleBuyClick = () => {
+    generalContext.openBuyWindow(uid);
+  };
+  const handleDeleteClick= async()=>{
+    await axios.delete(`http://localhost:3001/delete/${uid}`);
+    console.log(uid);    
+  }
+
+  return (
+    <span className="actions">
+      <div className="buy-sell-btns-cont">
+          <Tooltip
+            title="Buy (B)"
+            placement="top"
+            arrow
+            TransitionComponent={Grow}
+            onClick={handleBuyClick}
+          >
+            <button className="buy buy-btn">B</button>
+          </Tooltip>
+          <Tooltip
+            title="Sell (S)"
+            placement="top"
+            arrow
+            TransitionComponent={Grow}
+          >
+            <button className="sell sell-btn" onClick={handleDeleteClick}>S</button>
+          </Tooltip>
+        </div>
+      <span>    
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action" id="analytics-watchlist">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
